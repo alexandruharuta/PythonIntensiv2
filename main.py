6f29ec4e956f41bb61e8737b74e4ac2b335fb102
Alexandru Haruta <alexandruharuta@outlook.com>
2020-05-10 15:32:48 +0300

    Second commit'

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print('Insert two numbers')
+while True:
+    a = int(input())
+    b = int(input())
+    if (a > -1) and (b > -1):
+        print(a)
+        print(a + b)
+        print(a - b)
+        print(a * b)
+        print(a / b)
+        break
+    elif (a < 0) or (b < 0):
+        print("Numbers are not positive")
