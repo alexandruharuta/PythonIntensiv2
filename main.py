c7f883c6747b23c362f2e5b20e113c483995ee76
Alexandru Haruta <alexandruharuta@outlook.com>
2020-05-10 15:31:23 +0300

    First operations

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ while True:
     b = int(input())
     if (a > -1) and (b > -1):
         print(a)
-        print(a + b)
-        print(a - b)
+        print(a * b)
+        print(a / b)
         break
     elif (a < 0) or (b < 0):
         print("Numbers are not positive")
